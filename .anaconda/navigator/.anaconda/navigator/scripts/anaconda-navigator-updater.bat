4398166ce1dee0d91e4a2957d9278d6ee78b43eb
Yunju Cho <amy8883@naver.com>
2024-02-08 20:11:10 +0900

    index.html 추카

---
+++ .anaconda/navigator/.anaconda/navigator/scripts/anaconda-navigator-updater.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 949
+call C:\Users\amy88\anaconda3\Scripts\activate C:\Users\amy88\anaconda3
+navigator-updater --latest-version 2.1.4 --prefix C:\Users\amy88\anaconda3 >C:\Users\amy88\AppData\Roaming\SPB_Data\.anaconda\navigator\.anaconda\navigator\scripts\anaconda-navigator-updater-out-1.txt 2>C:\Users\amy88\AppData\Roaming\SPB_Data\.anaconda\navigator\.anaconda\navigator\scripts\anaconda-navigator-updater-err-1.txt
---
+++ .bash_history
@@ -0,0 +1,3 @@
+git config --global user.name "Yunju Cho"
+git config --global user.email "amy8883@naver.com"
+git config --list
